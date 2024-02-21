d243b32d27b8a63f8e13afdf15ba92b979f50041
Xujing <guoxujing@njust.edu.cn>
2024-02-21 11:35:34 +0800

    start

--- start.py
+++ start.py
@@ -2,8 +2,8 @@ import os
 
 from openxlab.model import download
 
-# download(model_repo='YOLOv10086/internlm',
-#         output='./adapter')
+download(model_repo='YOLOv10086/internlm',
+        output='./adapter')
 
 os.system('streamlit run web_demo.py --server.address=0.0.0.0 --server.port 7860')
 
