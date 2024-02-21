b0535ab68e166f99f58bbcab4dc8d85099a97f29
Xujing <guoxujing@njust.edu.cn>
2024-02-21 10:34:06 +0800

    3

--- start.py
+++ start.py
@@ -5,7 +5,8 @@ from openxlab.model import download
 os.system("git clone https://kkgithub.com/InternLM/InternLM.git")
 os.system("mv web_demo.py ./InternLM/")
 
-download(model_repo='YOLOv10086/internlm', )
+download(model_repo='YOLOv10086/internlm',
+        output='./InternLM/internlm)
 
-os.system('streamlit run InternLM/web_demo.py --server.address=0.0.0.0 --server.port 7860')
+os.system('cd ./InternLM && streamlit run web_demo.py --server.address=0.0.0.0 --server.port 7860')
 
