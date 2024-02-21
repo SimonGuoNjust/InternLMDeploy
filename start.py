b32cf70bef6cd52196d6396c2f3a96b36fe6deb1
Xujing <guoxujing@njust.edu.cn>
2024-02-21 10:45:23 +0800

    4

--- start.py
+++ start.py
@@ -6,7 +6,7 @@ os.system("git clone https://kkgithub.com/InternLM/InternLM.git")
 os.system("mv web_demo.py ./InternLM/")
 
 download(model_repo='YOLOv10086/internlm',
-        output='./InternLM/internlm)
+        output='./InternLM/internlm')
 
 os.system('cd ./InternLM && streamlit run web_demo.py --server.address=0.0.0.0 --server.port 7860')
 
